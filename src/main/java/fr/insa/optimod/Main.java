aa397d84d85ecf41752b04e7c025c16c9d5eacd6
robin <robin.renous83@gmail.com>
2025-11-17 11:05:17 +0100

    ajout xml

--- src/main/java/fr/insa/optimod/Main.java
+++ src/main/java/fr/insa/optimod/Main.java
@@ -3,9 +3,13 @@ package fr.insa.optimod;
 import fr.insa.optimod.controleur.Controleur;
 import fr.insa.optimod.modele.Carte;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
         Controleur controleur = new Controleur();
-        Carte carte = controleur.initialiserCarte("xml/ada.xml");
+        File f = new File("C:\\Users\\robin\\IdeaProjects\\optimod-lyon\\src\\main\\ressources\\xml\\petitPlan.xml");
+        System.out.println(f.exists());
+        Carte carte = controleur.initialiserCarte("C:\\Users\\robin\\IdeaProjects\\optimod-lyon\\src\\main\\ressources\\xml\\petitPlan.xml");
     }
 }
