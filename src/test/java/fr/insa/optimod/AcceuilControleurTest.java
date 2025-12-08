7c04c7139c0c1ddd886f0eea3b20279ba2955553
Raphaël <letourneur.raphael@gmail.com>
2025-12-08 09:40:10 +0100

    test AcceuilControleur

---
+++ src/test/java/fr/insa/optimod/AcceuilControleurTest.java
@@ -0,0 +1,54 @@
+package fr.insa.optimod;
+
+import fr.insa.optimod.controleur.AccueilControleur;
+import fr.insa.optimod.controleur.Controleur;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AcceuilControleurTest {
+
+    static AccueilControleur controleur;
+    static File grandPlan;
+    static File demande;
+    static File tournee;
+
+
+    @BeforeAll
+    public static void setUpClass(){
+        controleur = new AccueilControleur();
+        String grandPlanPath = AcceuilControleurTest.class.getResource("/xml/grandPlan.xml").getPath();
+        String demandeGrand = AcceuilControleurTest.class.getResource("/xml/demandeGrand9.xml").getPath();
+        String tourneePath = AcceuilControleurTest.class.getResource("/LivreurDay.pdf").getPath();
+        assertNotNull(grandPlanPath, "Fichier introuvable");
+        assertNotNull(demandeGrand, "Fichier introuvable");
+        assertNotNull(tourneePath, "Fichier introuvable");
+        grandPlan = new File(grandPlanPath);
+        demande = new File(demandeGrand);
+        tournee = new File(tourneePath);
+
+    }
+
+    @Test
+    public void validTestXml() {
+        System.out.println("Test valid: ");
+        assertTrue(controleur.isValidXML(grandPlan));
+    }
+
+    @Test
+    public void wrongTestXml() {
+        System.out.println("Test wrong: ");
+        assertFalse(controleur.isValidXML(demande));
+    }
+
+    @Test
+    public void wrongTestPdf() {
+        System.out.println("Test wrong 2: ");
+        assertFalse(controleur.isValidXML(tournee));
+    }
+
+}
